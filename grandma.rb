08c33d8ddab5930f0b2e7ef1042bbdd347817936
Herman <hrivas5726@aol.com>
2019-01-14 12:40:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,6 +14,8 @@
 
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase 
-    return HUH?! SPEAK UP, SONNY!
-  elsif
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase.upcase
+    return "NO, NOT SINCE 1938!"
+    
 end
